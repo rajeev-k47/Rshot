646f6249c8f1d109bad188f4adb28947801ce8f6
rajeev-k47 <rajeevkumawat2007@outlook.com>
2024-08-04 19:01:03 +0530

    <Refac:Defines init Routes>?

--- app/src/main/java/net/runner/rshot/MainActivity.kt
+++ app/src/main/java/net/runner/rshot/MainActivity.kt
@@ -1,10 +1,14 @@
 package net.runner.rshot
 
+import LoginScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import net.runner.rshot.composables.MainScreen
 import net.runner.rshot.ui.theme.RshotTheme
@@ -26,7 +30,18 @@ class MainActivity : ComponentActivity() {
                         color = MaterialTheme.colorScheme.background
                     )
                 }
-                MainScreen()
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = "LoginScreen"
+                ) {
+                    composable(route = "LoginScreen") {
+                       LoginScreen(navController)
+                    }
+                    composable(route = "MainScreen") {
+                        MainScreen()
+                    }
+                }
             }
 
          }
