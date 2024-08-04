bc30dc755e09b1b2156adf7b47df8d69b337edb8
rajeev-k47 <rajeevkumawat2007@outlook.com>
2024-08-04 19:01:39 +0530

    <*/*>?

---
+++ app/src/main/java/net/runner/rshot/MainViewModel.kt
@@ -0,0 +1,26 @@
+package net.runner.rshot
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class MainViewModel : ViewModel() {
+    //first state whether the search is happening or not
+    private val _isSearching = MutableStateFlow(false)
+    val isSearching = _isSearching.asStateFlow()
+
+    //second state the text typed by the user
+    private val _searchText = MutableStateFlow("")
+    val searchText = _searchText.asStateFlow()
+
+    fun onSearchTextChange(text: String) {
+        _searchText.value = text
+    }
+
+    fun onToogleSearch() {
+        _isSearching.value = !_isSearching.value
+        if (!_isSearching.value) {
+            onSearchTextChange("")
+        }
+    }
+}
