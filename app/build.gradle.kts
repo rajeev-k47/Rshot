7804e5006f78b978ba2be86fc3a5a5742d89e568
rajeev-k47 <rajeevkumawat2007@outlook.com>
2024-08-07 07:22:42 +0530

    <*/*>?

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,9 +80,6 @@ dependencies {
     implementation(libs.firebase.analytics)
     implementation (libs.accompanist.systemuicontroller)
 
-    implementation(libs.androidx.camera.camera2)
-    implementation(libs.androidx.camera.lifecycle)
-    implementation(libs.androidx.camera.view)
 
     implementation(libs.coil.compose)
 
